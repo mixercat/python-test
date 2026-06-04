d811163354125d897ddfef9bdfbc22eeeef31fb0
mixercat <mixercat47@gmail.com>
2026-06-04 16:11:38 +0700

    Create BooleanOpertorExample.py

---
+++ BooleanOpertorExample.py
@@ -0,0 +1,10 @@
+print("Welcome to the party")
+print("------------------------------")
+Age = int(input("Enter your age : "))
+
+if Age > 18:
+    print("You can enter the party")
+else:
+    print("You cannot enter the party because you are underage")
+
+print("------------------------------")
