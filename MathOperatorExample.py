180ca693341c03dd4efd44dd81a024b7fcee572c
mixercat <mixercat47@gmail.com>
2026-06-04 16:11:43 +0700

    Create MathOperatorExample.py

---
+++ MathOperatorExample.py
@@ -0,0 +1,12 @@
+money = int(input("Enter your money : "))
+Income_per_day = int(input("Enter the Income per day : "))
+Mom = 18000
+
+Income_per_month = Income_per_day * 22
+
+print("Money : " + str(money))
+print("Income per month : " + str(Income_per_month))
+print("Mom : " + str(Mom))
+print("------------------------------")
+print("Total : " + str(money + Income_per_month + Mom))
+print("------------------------------")
