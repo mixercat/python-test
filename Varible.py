4c388810b46fce0622ae3f1c8bf9965e0c6c60e7
mixercat <mixercat47@gmail.com>
2026-06-04 16:11:45 +0700

    Create Varible.py

---
+++ Varible.py
@@ -0,0 +1,9 @@
+Name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+tall = float(input("Enter your tall: "))
+weight = float(input("Enter your weight: "))
+
+print("Your name is " + Name)
+print("Your age is " + str(age))
+print("Your tall is " + str(tall))
+print("Your weight is " + str(weight))
