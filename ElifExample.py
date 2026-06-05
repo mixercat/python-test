e67e4ec9fd77fb6e40e58d06786825a55d727245
mixercat <mixercat47@gmail.com>
2026-06-05 22:04:47 +0700

    Create ElifExample.py

---
+++ ElifExample.py
@@ -0,0 +1,20 @@
+x = int(input("Enter a Grade : "))
+
+if x >= 80 :
+    print("Grade A")
+elif x >= 75 :
+    print("Grade B+")
+elif x >= 70 :
+    print("Grade B")
+elif x >= 65 :
+    print("Grade C+")
+elif x >= 60 :
+    print("Grade C")
+elif x >= 55 :
+    print("Grade D+")
+elif x >= 50 :
+    print("Grade D")
+else :    
+    print("Grade F")
+
+print()
