92f639a72f3647290f29fa126f5fcd98732d3435
mixercat <mixercat47@gmail.com>
2026-06-10 10:57:43 +0700

    Create PlayGround2.py

---
+++ PlayGround2.py
@@ -0,0 +1,33 @@
+rent = int(input())
+water = int(input())
+elec = int(input())
+net = int(input())
+phone = int(input())
+
+price = 2000
+total = price - (rent + water + elec + net + phone)
+
+if total >= 10:
+    All = 10
+    name = "chewy noodles"
+elif total >= 6:
+    All = 6
+    name = "instant noodles"
+elif total >= 2:
+    All = 2
+    name = "soup"
+else: 
+    All = 0
+    name = "drinking water"
+
+print("RENT  :", rent, "฿")
+print("WATER :", water, "฿")
+print("ELEC  :", elec, "฿")
+print("NET   :", net, "฿")
+print("PHONE :", phone, "฿")
+print("TOTAL :", rent, "+", water, "+", elec, "+", net, "+", phone)
+print("        =", rent + water + elec + net + phone, "฿")
+print("SELECT:", name)
+print("        =", All, "฿")
+print("SAVING:", price, "-", (rent + water + elec + net + phone), "-", All)
+print("      =", total - All, "฿")
