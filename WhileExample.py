22e8fbfd053098e6130f86937d20c51da35f49f6
mixercat <mixercat47@gmail.com>
2026-06-10 14:32:20 +0700

    Create WhileExample.py

---
+++ WhileExample.py
@@ -0,0 +1,11 @@
+while True :
+    Username = input("Enter your Username")
+    Password = input("Enter your Password")
+
+    if Username == "mixercat" and Password == "1234" :
+        print("---------------------------")
+        print("Welcome Sir")
+        print("---------------------------")
+        break
+    else : 
+        print("error")
