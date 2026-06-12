0fe88e24ba267cfac182bfa8f7704a96edd98c7e
mixercat <mixercat47@gmail.com>
2026-06-12 16:56:57 +0700

    Create ForLoopExample-2.py

---
+++ ForLoopExample-2.py
@@ -0,0 +1,10 @@
+inputRound = int(input("Please Enter Number : "))
+sum = 0
+
+for x in range(inputRound):
+        inputNumber = int(input("x" + str(x+1) + ":"))
+        print(x,": ",inputNumber)
+        sum += inputNumber
+
+print("Total",sum)
+
