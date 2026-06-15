eef44285b5e2861d260fd3095519b664dd1c8c69
mixercat <mixercat47@gmail.com>
2026-06-15 15:45:03 +0700

    Create FunctionExample2.py

---
+++ FunctionExample2.py
@@ -0,0 +1,10 @@
+def Number() :
+    num1 = int(input("Add Number1 :"))
+    num2 = int(input("Add Number2 :"))
+    print(num1 + num2)
+
+def Prime(x) :
+    num3 = x * 50
+    print(num3)
+
+Prime(50)
