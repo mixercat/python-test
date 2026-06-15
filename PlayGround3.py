0ba62679c59366dcca46fcb0e4eaa890d366b4ce
mixercat <mixercat47@gmail.com>
2026-06-15 14:11:41 +0700

    Update PlayGround3.py

--- PlayGround3.py
+++ PlayGround3.py
@@ -1,4 +1,19 @@
-x = int(input("enter a number"))
-for i in range(1,x+1):
-    print(i)
-    print(i*"*")
+min_val = int(input())
+max_val = int(input())
+
+prime_list = []
+
+for num in range(min_val, max_val + 1):
+    if num > 1:
+        is_prime = True
+        
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False  
+                break 
+                
+        if is_prime:
+            prime_list.append(num)
+
+print(f"found: {len(prime_list)}")
+print(prime_list)
