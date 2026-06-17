5c1dfe7f60a509489c29c83325e8827f07bc45a5
mixercat <mixercat47@gmail.com>
2026-06-17 13:08:00 +0700

    Create TupleExample.py

---
+++ TupleExample.py
@@ -0,0 +1,13 @@
+Name1 = ("Mix","Aum","Mark","Mae")
+Name2 = ("Lann","Yus","film")
+Name3 = Name1 + Name2
+
+for i in Name3 :
+    print("Mr."+i)
+
+if "Yus" in Name1 :
+    print("Yus : In name1")
+elif "Yus" in Name2 :
+    print("Yus : In name2")
+else :
+    print("None")
