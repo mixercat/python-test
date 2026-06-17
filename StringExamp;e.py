14034c93905c4b10642ed4ace9248bcd46b5487b
mixercat <mixercat47@gmail.com>
2026-06-17 11:37:18 +0700

    Create StringExamp;e.py

---
+++ StringExamp;e.py
@@ -0,0 +1,9 @@
+name = input().capitalize()
+lastname = input().capitalize()
+print("welcome %s %s"%(name,lastname))
+
+text = "mixercat"
+textformat ="Welcome %s"%(text)
+print(textformat.center(22,"-"))
+
+print(len(name))
