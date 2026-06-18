bc8dde57c263aa4360a45c448151e6c8c1735ff7
mixercat <mixercat47@gmail.com>
2026-06-18 10:03:43 +0700

    Create DictFunctionExample.py

---
+++ DictFunctionExample.py
@@ -0,0 +1,5 @@
+words = {"Cat" : "เเมววว","Dog" : "สุรโฮ่ง","Cow" : "วัวน่ะจ้ะ","Ant" : "เเอ้นเเมน","Mosue" : "มิกกี้เม้า"}
+print(words.items())
+print(words.keys())
+for i in words.values() :
+    print(i)
