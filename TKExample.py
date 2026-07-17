42586458d220ddfbc1408db3a9d1ca1607f6733d
mixercat <217460414+mixercat@users.noreply.github.com>
2026-07-17 14:06:01 +0700

    Update TKExample.py

--- TKExample.py
+++ TKExample.py
@@ -5,6 +5,8 @@ def sayHello():
 
 main_window = tkinter.Tk()
 main_window.title("Programming Example")
-button = tkinter.Button(main_window, text="Click Me Please", command=sayHello)
-button.place(x=50, y=50)
+button = tkinter.Button(main_window, text="Click Me Please1", command=sayHello)
+button.place(x=250, y=250)
+button2 = tkinter.Button(main_window, text="Click Me Please2", command=sayHello)
+button2.place(x=50, y=50)
 main_window.mainloop()
