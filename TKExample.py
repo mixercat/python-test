57d070eb8230adddbb60787c1b7f83cd2bbccc2d
mixercat <217460414+mixercat@users.noreply.github.com>
2026-07-18 16:00:10 +0700

    Update TKExample.py

--- TKExample.py
+++ TKExample.py
@@ -8,8 +8,8 @@ def sayHello2():
 
 main_window = tkinter.Tk()
 main_window.title("Programming Example")
-button = tkinter.Button(main_window, text="Click Me Please1", command=sayHello,width=100,fg="blue",bg="yellow").grid(row=0, column=0)   
-button2 = tkinter.Button(main_window, text="Click Me Please2", command=sayHello2,width=100,fg="blue",bg="yellow").grid(row=1, column=0)
-button3 = tkinter.Button(main_window, text="Click Me Please3", command=sayHello2,width=100,fg="blue",bg="yellow").grid(row=2, column=0)
-label = tkinter.Label(main_window, text="Hello Adirut",width=100,fg="blue",bg="gray").grid(row=3, column=0)
+button = tkinter.Button(main_window, text="Click Me Please1", command=sayHello,width=100,fg="blue",bg="yellow",font=("Arial", 18)).grid(row=0, column=0)   
+button2 = tkinter.Button(main_window, text="Click Me Please2", command=sayHello2,width=100,fg="blue",bg="yellow",font=("Arial", 18)).grid(row=1, column=0)
+button3 = tkinter.Button(main_window, text="Click Me Please3", command=sayHello2,width=100,fg="blue",bg="yellow",font=("Arial", 18)).grid(row=2, column=0)
+label = tkinter.Label(main_window, text="Hello Adirut",width=100,fg="blue",bg="gray",font=("Arial", 18)).grid(row=3, column=0)
 main_window.mainloop()
