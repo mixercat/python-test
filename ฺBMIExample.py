05bf7d0fe5c3d1289c760a6625e92841a89af359
mixercat <217460414+mixercat@users.noreply.github.com>
2026-07-18 17:17:07 +0700

    Update ฺBMIExample.py

--- ฺBMIExample.py
+++ ฺBMIExample.py
@@ -6,21 +6,37 @@ def calculate_bmi():
     weight = float(textWeight.get())
     bmi = weight / (height_cm ** 2)
     resultLabel.config(text=f"BMI: {bmi}")
-    print(f"Height: {height} cm, Weight: {weight} kg, BMI: {bmi}")
+    resultCriterionLabel.config(text=f"Criterion: {criterion(bmi)}")
+    print(f"Height: {height} cm, Weight: {weight} kg, BMI: {bmi}, Criterion: {criterion(bmi)}")
+
+def criterion(bmi):
+        if bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= bmi < 24.9:
+            return "Normal weight"
+        elif 25 <= bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obesity"
 
 main = Tk()
 main.title("BMI Calculator")
 
-labelHeight = Label(main, text="Height (cm):", font=("Arial", 12)).grid(row=0, column=0)
-textHeight = Entry(main, font=("Arial", 12))
+labelHeight = Label(main, text="Height (cm):", font=("Arial", 20), width=20).grid(row=0, column=0)
+textHeight = Entry(main, font=("Arial", 20))
 textHeight.grid(row=0, column=1)
 
-labelWeight = Label(main, text="Weight (kg):", font=("Arial", 12)).grid(row=1, column=0)
-textWeight = Entry(main, font=("Arial", 12))
+labelWeight = Label(main, text="Weight (kg):", font=("Arial", 20), width=20).grid(row=1, column=0)
+textWeight = Entry(main, font=("Arial", 20))
 textWeight.grid(row=1, column=1)
 
-buttonCalculate = Button(main, text="Calculate BMI", font=("Arial", 12), command=calculate_bmi)
+buttonCalculate = Button(main, text="Calculate BMI", font=("Arial", 20), command=calculate_bmi)
 buttonCalculate.grid(row=2, column=0, columnspan=2)
-resultLabel = Label(main, text="BMI: ", font=("Arial", 12))
+
+resultLabel = Label(main, text="BMI: ", font=("Arial", 20), width=20)
 resultLabel.grid(row=3, column=0, columnspan=2)
+
+resultCriterionLabel = Label(main, text="Criterion: ", font=("Arial", 20), width=20)
+resultCriterionLabel.grid(row=4, column=0, columnspan=2)
+
 main.mainloop()
